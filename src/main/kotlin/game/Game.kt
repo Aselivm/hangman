218f64c117753bbd39f449c429a1d1184c209464
Stepan Primshits <step6199@gmail.com>
2024-09-22 00:32:39 +0300

    fix display

--- src/main/kotlin/game/Game.kt
+++ src/main/kotlin/game/Game.kt
@@ -6,7 +6,7 @@ class Game(val word: String, var remainingAttempts: Int, val difficulty: Difficu
     private var guessedLettersArray: MutableList<Char>
 
     val maskedWord: String
-        get() = maskedWordArray.joinToString(", ")
+        get() = maskedWordArray.joinToString(" ")
 
     val guessedLetters: String
         get() = guessedLettersArray.joinToString(", ")
